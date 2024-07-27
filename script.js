deeec298bfecf5cdfc6f2eec0564c5eb6c18a0a0
Edward Omopariola <femiedjr@gmail.com>
2024-07-27 09:18:49 -0400

    touch up on js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getOptions() {
   const myProducts = document.getElementById("appleProduct"); //Using the ID element
-  const totalItems = document.getElementById("appleProduct").length; //with the length method, i can get the total item
-  console.log(totalItems); //
+  // const totalItems = document.getElementById("appleProduct").length; //with the length method, i can get the total item
+  console.log(myProducts); //
   const options = myProducts.options[myProducts.selectedIndex].value; //with the value method, i can get the numbers of product
   let stringOfOptions = ""; //Using the let variable with stringOfOptions and giving it an empty string
 // Using the for loop to loop through myProduct
